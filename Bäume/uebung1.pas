fe2ff1be36b4e310e3ba14d14bc4a0fc684e0005
ibiBgOR <plasmaosc@aol.com>
2013-12-19 18:26:51 +0100

    Baum traversierung

--- Bäume/uebung1.pas
+++ Bäume/uebung1.pas
@@ -5,75 +5,72 @@ type
 	node = Record
 		left:link;
 		right:link;
-		key_nachname:String;
-		key_vorname:String;
+		key_name:String;
 	End;
 
 var
 	root:link; i:Integer; test:String;
 
 (* Ein Element in den Baum einfügen. *)
-procedure insertNode(var n:link; nachname:String; vorname:String);
+procedure insertNode(var n:link; name:String);
 	Begin
 		IF n = NIL Then
 			Begin
 				new(n);
 				n^.left:=NIL;
 				n^.right:=NIL;
-				n^.key_nachname:=nachname;
-				n^.key_vorname:=vorname;
+				n^.key_name:=name;
 			End
 		ELSE
 			Begin
-				IF nachname < n^.key_nachname Then
+				IF name < n^.key_name Then
 					Begin
-						insertNode(n^.left, nachname, vorname);
+						insertNode(n^.left, name);
 					End
-				ELSE IF nachname > n^.key_nachname Then
+				ELSE IF name > n^.key_name Then
 					Begin
-						insertNode(n^.right, nachname, vorname);
+						insertNode(n^.right, name);
 					End
 				ELSE
 					writeln('Schluessel schon vorhanden');
 			End;
 	End;
 	
-(* Den Baum ausgeben. *)
+(* Den Baum ausgeben. (Ebenfalls ein Traversierungsalgorithmus, jedoch preorder) *)
 procedure printTree(var start:link; name:String);
 	Begin
-		writeln(name + ' ' + start^.key_nachname);
+		writeln(name + ' ' + start^.key_name);
 		IF NOT (start^.left = NIL) Then
-			printTree(start^.left,'Linker Teilbaum von ' + start^.key_nachname);
+			printTree(start^.left,'Linker Teilbaum von ' + start^.key_name);
 		IF NOT (start^.right = NIL) Then
-			printTree(start^.right,'Rechter Teilbaum von ' + start^.key_nachname);
+			printTree(start^.right,'Rechter Teilbaum von ' + start^.key_name);
 	End;
 
 (* Die 10 Namen direkt von dem Nutzer in den Baum einfügen. *)
 procedure insertUsersInput();
 	var
-		nachname, vorname:String;
+		name:String;
 	Begin
-		writeln('Geben Sie einen Nachnamen ein: ');
-		readln(nachname);
-		writeln('Nun geben Sie einen Vornamen ein: ');
-		readln(vorname);
-		insertNode(root, nachname, vorname)
+		writeln('Geben Sie einen Namen ein (Nachname Vorname): ');
+		readln(name);
+		insertNode(root, name)
 	End;
 	
+(* In dem Baum nach einem Namen suchen. *)
 function searchName(var name:String; lnk:link):String;
 	Begin
-		IF name = lnk^.key_nachname Then
+		IF name = lnk^.key_name Then
 			Begin
 				searchName:=name + ' ist enthalten.';
 			End
-		ELSE IF name < lnk^.key_nachname Then
+		ELSE IF name < lnk^.key_name Then
 			Begin
 				IF NOT (lnk^.left = NIL) Then
 					searchName(name, lnk^.left)
 				ELSE
 					searchName:=name + ' ist nicht enthalten.'
 			End
-		ELSE IF name > lnk^.key_nachname Then
+		ELSE IF name > lnk^.key_name Then
 			Begin
 				IF NOT (lnk^.right = NIL) Then
 					searchName(name, lnk^.right)
@@ -81,18 +78,32 @@ function searchName(var name:String; lnk:link):String;
 					searchName:=name + ' ist nicht enthalten.'			
 			End;
 	End;
+	
+(* Den Baum traversieren. (Jeden Knoten durchgehen) (inorder) *)
+procedure traverseTreeInorder(lnk:link);
+	Begin
+		IF NOT (lnk = NIL) THEN
+			BEGIN
+				traverseTreeInorder(lnk^.left);
+				writeln(lnk^.key_name);
+				traverseTreeinorder(lnk^.right);
+			END;
+	End;
 
 (* Mainprogram *)	
-Begin	
+Begin		
 	(* 10 Nutzereingaben für den Baum *)
 	FOR i:=0 TO 10 DO
 		insertUsersInput();
 		
 	(* Baum ausgeben *)
-	printTree(root, 'root');
+	printTree(root, 'root'); 
 	
 	(* Einen Namen suchen. *)
 	test:='Haller';
-	insertNode(root, 'Heller', 'Hans');
+	insertNode(root, 'Heller Hans');
 	writeln(searchName(test, root));
+	
+	(* Traversieren inorder *)
+	traverseTreeInorder(root);
 End.
